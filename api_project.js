074e26ae2e720508860b55ef679cfd91c3d1731c
meriadec <meriadec.pillet@gmail.com>
2016-12-04 20:17:23 +0100

    deploy

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Bot Connector API Documentation",
+  "version": "0.0.0",
+  "title": "Bot Connector API Documentation",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-12-04T19:17:22.952Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
