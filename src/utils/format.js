457dcf2a0f42fd615a0a32f50d665edcac6d8708
jhoudan <jerom.houdan@gmail.com>
2017-02-07 00:21:11 +0100

    [UPD] format checker utils

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,13 +1,13 @@
 import _ from 'lodash'
 
-export const messageTypes = ['text', 'picture', 'video', 'quickReplies', 'card']
+export const messageTypes = ['text', 'picture', 'video', 'quickReplies', 'card', 'carouselle', 'audio']
 
 export function isValidFormatMessage (message) {
   if (!_.isObject(message)
-    || !message.type || !message.content
-    || messageTypes.indexOf(message.type) === -1) {
-    return false
-  }
+      || !message.type || !message.content
+      || messageTypes.indexOf(message.type) === -1) {
+      return false
+    }
 
   if (message.type === 'text' && !_.isString(message.content)) { return false }
   if (message.type === 'picture' && !_.isString(message.content)) { return false }
