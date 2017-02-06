e2b6145107ca1c231ae7a3d1acd6ca31b7574d2e
jhoudan <jerom.houdan@gmail.com>
2017-02-07 00:37:37 +0100

    refactor(model) replace Channel schema Mongoose ids by v4 UUID

--- src/models/Channel.model.js
+++ src/models/Channel.model.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose'
+import uuid from 'uuid/v4'
+import _ from 'lodash'
+
 import { getWebhookToken } from '../utils'
 
 const ChannelSchema = new mongoose.Schema({
-  bot: { type: mongoose.Schema.Types.ObjectId, ref: 'Bot', required: true },
+  _id: { type: String, default: uuidV4 },
+  bot: { type: String, ref: 'Bot', required: true },
   slug: { type: String, required: true },
   type: { type: String, required: true },
   token: String,
@@ -15,18 +19,25 @@ const ChannelSchema = new mongoose.Schema({
   timestamps: true,
 })
 
+async function generateUUID (next) {
+  if (this.isNew) {
+    while (await models.Channel.findOne({ _id: this._id })) {
+      this._id = uuidV4()
+    }
+  }
+  next()
+}
+
+ChannelSchema.pre('save', generateUUID)
+
 ChannelSchema.virtual('serialize').get(function () {
+  const filteredChannel = _.pickBy(this.toObject(), (value) => value)
+  delete filteredChannel._id
+
   return {
     id: this._id,
-    bot: this.bot,
-    slug: this.slug,
-    type: this.type,
-    token: this.token,
-    userName: this.userName,
-    apiKey: this.apiKey,
-    webhook: this.webhook,
-    isActivated: this.isActivated,
-    webhookToken: this.verifyToken = this.type === 'messenger' ? getWebhookToken(this._id, this.slug) : null,
+    ...filteredChannel,
+    webhookToken: this.type === 'messenger' ? getWebhookToken(this._id, this.slug) : undefined,
   }
 })
 
