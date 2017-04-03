f4b119506173c6c745ec4a2eb40e9d2fedee0d6f
jhoudan <jerom.houdan@gmail.com>
2017-04-03 13:56:01 +0200

    refactor(core) update Slack service

--- src/services/Slack.service.js
+++ src/services/Slack.service.js
@@ -1,93 +1,30 @@
-import { RtmClient, RTM_EVENTS } from '@slack/client'
 import request from 'superagent'
-
-import { ValidationError, ConnectorError } from '../utils/errors'
-import MessagesController from '../controllers/Messages.controller'
 
 import ServiceTemplate from './Template.service'
 import Logger from '../utils/Logger'
+import { BadRequestError, ConnectorError } from '../utils/errors'
 
 export default class SlackService extends ServiceTemplate {
-  // This static map will be used to stock all the socket connections to allow us to close connections when receiving PUT/DELETE on channels
-  static allRtm = new Map()
 
-  // This function launches all websocket clients for slack channels on startup
-  static onLaunch () {
-    return new Promise(resolve => {
-      Channel
-        .find({ type: 'slack' })
-        .then(channels => {
-          channels.forEach(c => SlackService.onChannelCreate(c))
-          return resolve()
-        })
-        .catch(err => {
-          Logger.error(`Error while handling slack socket: ${err}`)
-          return resolve()
-        })
-    })
+  static extractOptions (req) {
+    const { body } = req
+
+    return {
+      chatId: body.event.channel,
+      senderId: body.event.user,
+    }
+  }
+
+  static checkSecurity (req, res) {
+    res.status(200).send()
   }
 
   static checkParamsValidity (channel) {
     const { token } = channel
 
-    if (!token) { throw new ValidationError('token', 'missing') }
+    if (!token) { throw new BadRequestError('Parameter token is missing') }
 
     return true
-  }
-
-  /**
-   * Create a socket connection to Slack
-   */
-  static onChannelCreate (channel) {
-    if (channel.isActivated === false) {
-      return
-    }
-
-    const rtm = new RtmClient(channel.token, { logLevel: 'info' })
-
-    // Save rtm client in static map to close connection later
-    SlackService.allRtm.set(channel._id.toString(), rtm)
-    rtm.start()
-
-    rtm.on(RTM_EVENTS.MESSAGE, slackMessage => {
-      const opts = { chatId: slackMessage.channel, senderId: slackMessage.user }
-
-      if (!slackMessage.user) { return }
-      const user = rtm.dataStore.getUserById(slackMessage.user)
-      if (!user) { return }
-      const dm = rtm.dataStore.getDMByName(user.name)
-      if (!dm) { return }
-
-      // Start connector pipe for input message
-      MessagesController.pipeMessage(channel._id, slackMessage, opts)
-        .catch(err => {
-          Logger.error(`An error occured while handling message: ${err}`)
-          if (err instanceof ConnectorError) {
-            rtm.sendMessage(err.message, slackMessage.channel)
-          } else {
-            rtm.sendMessage('An error occured while sending message', slackMessage.channel)
-          }
-        })
-    })
-  }
-
-  /**
-   *  Close and open a Slack socket connection
-   */
-  static async onChannelUpdate (channel) {
-    SlackService.onChannelDelete(channel)
-    await SlackService.onChannelCreate(channel)
-  }
-
-  /**
-   * Delete the Slack socket connection
-   */
-  static onChannelDelete (channel) {
-    const rtm = SlackService.allRtm.get(channel._id.toString())
-    if (rtm) {
-      rtm.disconnect()
-      SlackService.allRtm.delete(channel._id.toString())
-    }
   }
 
   /**
@@ -99,27 +36,25 @@ export default class SlackService extends ServiceTemplate {
         channelType: 'slack',
       }
       let attachment = {}
-
-      if (message.file) {
-        if (message.file.mimetype.startsWith('image')) {
-          attachment = { type: 'picture', content: message.file.url_private }
-        } else if (message.file.mimetype.startsWith('video')) {
-          attachment = { type: 'picture', content: message.file.url_private }
+      if (message.event.file) {
+        if (message.event.file.mimetype.startsWith('image')) {
+          attachment = { type: 'picture', content: message.event.file.url_private }
+        } else if (message.event.file.mimetype.startsWith('video')) {
+          attachment = { type: 'picture', content: message.event.file.url_private }
         } else {
           return reject(new ConnectorError('Sorry but we don\'t handle such type of file'))
         }
       } else {
-        attachment = { type: 'text', content: message.text }
+        attachment = { type: 'text', content: message.event.text }
       }
       parsedMessage.attachment = attachment
       return resolve([conversation, parsedMessage, opts])
     })
   }
 
-  // Transforms a message from connector universal format to slack format
+    // Transforms a message from connector universal format to slack format
   static formatMessage (conversation, message) {
     const { type, content } = message.attachment
-
     let slackFormattedMessage = null
     switch (type) {
     case 'text':
@@ -131,7 +66,6 @@ export default class SlackService extends ServiceTemplate {
     case 'picture':
       slackFormattedMessage = { text: content }
       break
-    // Handle quickreplies as buttons
     case 'quickReplies':
       slackFormattedMessage = {
         text: content.title,
@@ -139,6 +73,7 @@ export default class SlackService extends ServiceTemplate {
       slackFormattedMessage.attachments = [{
         fallback: 'Sorry but I can\'t display buttons',
         attachment_type: 'default',
+        callback_id: 'callback_id',
         actions: content.buttons.map(button => {
           button.name = button.title
           button.text = button.title
@@ -149,12 +84,14 @@ export default class SlackService extends ServiceTemplate {
       }]
       break
     case 'card':
-      slackFormattedMessage = {
-        text: content.title,
-      }
+      slackFormattedMessage = {}
       slackFormattedMessage.attachments = [{
+        title: content.title,
+        text: content.subtitle,
+        image_url: content.imageUrl,
         fallback: 'Sorry but I can\'t display buttons',
         attachment_type: 'default',
+        callback_id: 'callback_id',
         actions: content.buttons.map(button => {
           button.name = button.title
           button.text = button.title
@@ -184,13 +121,13 @@ export default class SlackService extends ServiceTemplate {
         params = `${params}&attachments=${JSON.stringify(message.attachments)}`
       }
       request.post(`https://slack.com/api/chat.postMessage?${authParams}${params}`)
-        .end(err => {
-          if (err) {
-            Logger.error('Error while sending message to slack')
-            return reject(err)
-          }
-          resolve('Message sent')
-        })
+      .end((err) => {
+        if (err) {
+          Logger.error('Error while sending message to slack')
+          return reject(err)
+        }
+        resolve('Message sent')
+      })
     })
   }
 }
