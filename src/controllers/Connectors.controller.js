cd0cf52a1303d0db73b8911ec6d663930daefdb0
jhoudan <jerom.houdan@gmail.com>
2017-04-03 13:08:55 +0200

    refactor(core) update and rename Bot controller to Connector controller

---
+++ src/controllers/Connectors.controller.js
@@ -0,0 +1,84 @@
+import filter from 'filter-object'
+
+import {
+  NotFoundError,
+  BadRequestError,
+} from '../utils/errors'
+
+import {
+  renderOk,
+  renderCreated,
+  renderDeleted,
+} from '../utils/responses'
+
+const permittedAdd = '{url}'
+const permittedUpdate = '{url}'
+
+export default class ConnectorsController {
+
+  /**
+   * Create a new connector
+   */
+  static async createConnector (req, res) {
+    const payload = filter(req.body, permittedAdd)
+
+    const connector = new models.Connector(payload).save()
+
+    return renderCreated(res, {
+      results: connector.serialize,
+      message: 'Connector successfully created',
+    })
+  }
+
+  /**
+  * Show a connector
+  */
+  static getConnectorByBotId (req, res) {
+    const { connector_id } = req.params
+
+    const connector = await models.Connector.findById(connector_id)
+
+    if (!connector) { throw new NotFoundError('Connector') }
+
+    return renderOk(res, {
+      results: connector.serialize,
+      message: 'Connector successfully found',
+    })
+  }
+
+  /**
+  * Update a connector
+  */
+  static async updateConnectorByBotId (req, res) {
+    const { connector_id } = req.params
+
+    const connector = await models.Connector.findOneAndUpdate({ _id: currentBot.connector._id },
+      { $set: filter(req.body, permittedUpdate) }, { new: true }
+    ).populate('channels')
+
+    return renderOk(res, {
+      results: connector.serialize,
+      message: 'Connector successfully updated',
+    })
+  }
+
+  /**
+  * Delete a connector
+  */
+  static async deleteConnectorByBotId (req, res) {
+    const { connector_id } = req.params
+    const { currentBot } = req.user
+
+    const connector = await models.Connector.findById(connector_id)
+          .populate('channels conversations')
+
+    await Promise.all([
+      ...connector.conversations.map(c => c.remove()),
+      ...connector.channels.map(c => c.remove()),
+      connector.remove(),
+    ])
+
+    return renderDeleted(res)
+  }
+
+}
