7673e52f4627e72e5e574ac9cef157afd96ab937
jhoudan <jerom.houdan@gmail.com>
2017-04-03 11:12:34 +0200

    refactor(core) update Webhooks routes

--- src/routes/Webhooks.routes.js
+++ src/routes/Webhooks.routes.js
@@ -1,9 +1,7 @@
-import WebhooksController from '../controllers/Webhooks.controller.js'
-import * as webhookValidators from '../validators/Webhooks.validators.js'
-
 export default [
+
   /*
-  * This route is the webhook sharing wiht channel
+  * This route is the webhook shared with a channel
   * Depending on incomming request, it automatically detect which channel message is comming from.
   * In many cases, this webhook is automatically registered onto right channel (Kik for example).
   * Check our documentation for more info.
@@ -11,8 +9,8 @@ export default [
   {
     method: 'POST',
     path: '/webhook/:channel_id',
-    validators: [webhookValidators.forwardMessage],
-    handler: WebhooksController.forwardMessage,
+    validators: [],
+    handler: controllers.Webhooks.forwardMessage,
   },
 
   /*
@@ -22,8 +20,7 @@ export default [
   {
     method: 'GET',
     path: '/webhook/:channel_id',
-    validators: [webhookValidators.subscribeFacebookValidator],
-    handler: WebhooksController.subscribeFacebookWebhook,
+    validators: [],
+    handler: controllers.Webhooks.subscribeFacebookWebhook,
   },
-
 ]
