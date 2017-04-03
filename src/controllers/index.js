689d65422358885cb6df3a7575fb8b425ad73cff
jhoudan <jerom.houdan@gmail.com>
2017-04-03 13:45:21 +0200

    feat(core) add controllers#index

---
+++ src/controllers/index.js
@@ -0,0 +1,17 @@
+import App from './App.controller'
+import Connectors from './Connectors.controller'
+import Channels from './Channels.controller'
+import Messages from './Messages.controller'
+import Webhooks from './Webhooks.controller'
+import Conversations from './Conversations.controller'
+import Participants from './Participants.controller'
+
+export default {
+  App,
+  Connectors,
+  Channels,
+  Messages,
+  Webhooks,
+  Conversations,
+  Participants,
+}
