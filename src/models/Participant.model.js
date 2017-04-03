0c281f4762c27a5aec4a644482c65449bdd390cb
jhoudan <jerom.houdan@gmail.com>
2017-04-03 10:44:34 +0200

    refactor(core) update Participant model

--- src/models/Participant.model.js
+++ src/models/Participant.model.js
@@ -1,11 +1,24 @@
 import mongoose from 'mongoose'
+import uuidV4 from 'uuid/v4'
 
 const ParticipantSchema = new mongoose.Schema({
+  _id: { type: String, default: uuidV4 },
   senderId: String,
   isBot: { type: Boolean, default: false },
 }, {
   timestamps: true,
 })
+
+async function generateUUID (next) {
+  if (this.isNew) {
+    while (await models.Participant.findOne({ _id: this._id })) {
+      this._id = uuidV4()
+    }
+  }
+  next()
+}
+
+ParticipantSchema.pre('save', generateUUID)
 
 ParticipantSchema.virtual('serialize').get(function () {
   return {
