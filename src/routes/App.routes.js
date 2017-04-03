5abd7c010201ad5f67e96205b40550ea39181e86
jhoudan <jerom.houdan@gmail.com>
2017-04-03 10:52:55 +0200

    refactor(core) update Channel routes

--- src/routes/App.routes.js
+++ src/routes/App.routes.js
@@ -2,12 +2,14 @@ export default [
   {
     method: 'GET',
     path: '/',
+    validators: [],
     handler: controllers.App.index,
   },
 
   {
     method: 'POST',
     path: '/',
+    validators: [],
     handler: controllers.App.index,
   },
 ]
