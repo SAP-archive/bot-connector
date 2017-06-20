1a9214446dab8398945b6648bc3bd9355b33306e
jhoudan <jerom.houdan@gmail.com>
2017-06-20 17:57:46 +0200

    refactor(controller) minor updates on Connector

--- src/controllers/Connectors.controller.js
+++ src/controllers/Connectors.controller.js
@@ -1,24 +1,18 @@
+import _ from 'lodash'
 import filter from 'filter-object'
 
-import {
-  NotFoundError,
-} from '../utils/errors'
+import { NotFoundError, BadRequestError } from '../utils/errors'
+import { renderOk, renderCreated, renderDeleted } from '../utils/responses'
 
-import {
-  renderOk,
-  renderCreated,
-  renderDeleted,
-} from '../utils/responses'
-
-const permittedAdd = '{url}'
-const permittedUpdate = '{url}'
+const permittedAdd = '{url,isTyping}'
+const permittedUpdate = '{url,isTyping}'
 
 export default class ConnectorsController {
 
   /**
    * Create a new connector
    */
-  static async createConnector (req, res) {
+  static async create (req, res) {
     const payload = filter(req.body, permittedAdd)
 
     const connector = await new models.Connector(payload).save()
@@ -30,14 +24,16 @@ export default class ConnectorsController {
   }
 
   /**
-   * Show a connector
-   */
-  static async getConnectorByBotId (req, res) {
+  * Show a connector
+  */
+  static async show (req, res) {
     const { connector_id } = req.params
 
     const connector = await models.Connector.findById(connector_id)
 
-    if (!connector) { throw new NotFoundError('Connector') }
+    if (!connector) {
+      throw new NotFoundError('Connector')
+    }
 
     return renderOk(res, {
       results: connector.serialize,
@@ -45,17 +41,16 @@ export default class ConnectorsController {
     })
   }
 
-  /**
-   * Update a connector
-   */
-  static async updateConnectorByBotId (req, res) {
+  /*
+  * Update a connector
+  */
+  static async update (req, res) {
     const { connector_id } = req.params
 
-    const connector = await models.Connector.findOneAndUpdate({ _id: connector_id },
+    const connector = await global.models.Connector.findOneAndUpdate(
+      { _id: connector_id },
       { $set: filter(req.body, permittedUpdate) }, { new: true }
     ).populate('channels')
-
-    if (!connector) { throw new NotFoundError('Connector') }
 
     return renderOk(res, {
       results: connector.serialize,
@@ -64,15 +59,17 @@ export default class ConnectorsController {
   }
 
   /**
-   * Delete a connector
-   */
-  static async deleteConnectorByBotId (req, res) {
+  * Delete a connector
+  */
+  static async delete (req, res) {
     const { connector_id } = req.params
 
     const connector = await models.Connector.findById(connector_id)
-          .populate('channels conversations')
+      .populate('channels conversations')
 
-    if (!connector) { throw new NotFoundError('Connector') }
+    if (!connector) {
+      throw new NotFoundError('Connector')
+    }
 
     await Promise.all([
       ...connector.conversations.map(c => c.remove()),
